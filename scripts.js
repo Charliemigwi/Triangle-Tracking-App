5b3982332bbd78f806fae53bc56982e3b748bc73
Charlie Migwi <charliemigwi@gmail.com>
2019-06-21 15:05:58 +0300

    define result variables and check triangle sides

--- scripts.js
+++ scripts.js
@@ -10,7 +10,25 @@ var triangleType = function(){
   if(isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
     output.textContent = "Please Input Numbers for all Fields!";
   } else {
-        
-  }
-
+  //Initialize add() function
+  function add(number1, number2){
+  return number1 + number2;
+};
+    var resultA = (sideB + sideC);
+    var resultB = (sideA + sideC);
+    var resultC = (sideA + sideB);
+if (resultA <= sideA || resultB <= sideB || resultC <= sideC ){
+    output.textContent="You cannot form a Triangle from those values!!";
+}else if(sideA === sideB && sideB != sideC){
+    output.textContent="The Values Entered are for a Isosceles Triangle";
+}else if(sideA === sideC && sideC != sideB){
+    output.textContent="The Values Entered are for a Isosceles Triangle";
+}else if(sideB === sideC && sideC != sideA){
+    output.textContent="The Values Entered are for a Isosceles Triangle";
+} else if(sideA === sideB && sideB === sideC) {
+    output.textContent= "The Values Entered are for an Equilateral Triangle";
 }
+else {
+    output.textContent ="The Values Entered are for a Scalene Triangle";
+}
+};
